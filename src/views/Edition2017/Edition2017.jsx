b90371751ea46bff9531b3aa746a404c20827419
Krystian Wawer <arnechos@gmail.com>
2017-10-05 20:48:41 +0200

    Add files via upload

--- src/views/Edition2017/Edition2017.jsx
+++ src/views/Edition2017/Edition2017.jsx
@@ -1,8 +1,16 @@
-import React, { Component } from 'react';
+﻿import React, { Component } from 'react';
 import Gallery from './Gallery/Gallery';
 import Partners from './Partners/Partners';
 import { Container } from '../../utils/constants/styledComponents';
+import 'autotrack'
 
+ga('create', 'GTM-NM7RXM', 'auto');
+
+ga('require', 'eventTracker');
+ga('require', 'outboundLinkTracker');
+ga('require', 'urlChangeTracker');
+
+ga('send', 'pageview');
 export default class Edition2017 extends Component {
   render() {
     return (

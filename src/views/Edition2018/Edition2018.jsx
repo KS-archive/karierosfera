b06b2e4cc2165b78347652c33963bedf0e3bf1ee
Krystian Wawer <arnechos@gmail.com>
2017-10-05 20:48:58 +0200

    Add files via upload

--- src/views/Edition2018/Edition2018.jsx
+++ src/views/Edition2018/Edition2018.jsx
@@ -1,8 +1,16 @@
-import React, { Component } from 'react';
+﻿import React, { Component } from 'react';
 import NewArea from './NewArea/NewArea';
 import FutureStages from './FutureStages/FutureStages';
 import { Container } from '../../utils/constants/styledComponents';
+import 'autotrack'
 
+ga('create', 'GTM-NM7RXM', 'auto');
+
+ga('require', 'eventTracker');
+ga('require', 'outboundLinkTracker');
+ga('require', 'urlChangeTracker');
+
+ga('send', 'pageview');
 export default class Edition2018 extends Component {
   render() {
     return (

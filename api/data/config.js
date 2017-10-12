ec23c3c37d8b04a8518a41104fd39b74732b8672
KonradSzwarc <konrad.szwarc@outlook.com>
2017-10-13 01:32:45 +0200

    Change email for ambassadors form

--- api/data/config.js
+++ api/data/config.js
@@ -3,7 +3,7 @@ module.exports = {
   EMAIL_PASSWORD: 'karierosfera',
   EMAIL_HOST: 'smtp.gmail.com',
   MAIL_CONTACT: 'kontakt@karierosfera.pl',
-  MAIL_AMBASSADOR: 'cvambasadorzy@karierosfera.pl',
+  MAIL_AMBASSADOR: 'cvambasadorzy@wiggor.pl',
   MAIL_NEWIDEA: 'kontakt@karierosfera.pl',
   MAIL_NEWSLETTER: 'kontakt@karierosfera.pl',
   STATIC_MAX_AGE: 7 * 24 * 60 * 60 * 1000,
